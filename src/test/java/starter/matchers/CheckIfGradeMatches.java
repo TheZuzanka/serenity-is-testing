4425f7449a9e53dbea202d5b3d1135c3d4cf3448
TheZuzanka <wlachovska.zuzana@gmail.com>
2020-01-27 22:18:19 +0100

    vytvorený matcher pre kontrolu, či sa v zadanom období zhoduje známka v e-indexe: prerobené na dve separátne funkcie

---
+++ src/test/java/starter/matchers/CheckIfGradeMatches.java
@@ -0,0 +1,91 @@
+package starter.matchers;
+
+import net.serenitybdd.core.annotations.findby.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CheckIfGradeMatches {
+    public void check(String grade, String subject){
+        WebElement rowContainingSubject;
+        String finalGrade;
+        boolean containsText;
+
+        //hladam spravny riadok
+        rowContainingSubject = findSubjectRow(subject);
+
+        //uz mam najdeny spravny riadok, urobim v nom co potrebujem: znamka je v stlpci c. 6
+        finalGrade = findFinalGrade(rowContainingSubject);
+
+        if(finalGrade.contains("(" + grade + ")")){
+            System.out.println("E-study record contains expected grade \"" + grade + "\"");
+            containsText = true;
+        }
+        else{
+            System.out.println("E-study record DOESN'T contain expected grade \"" + grade + "\"");
+            System.out.println("Expected: " + grade + ", Is: " + finalGrade);
+            containsText = false;
+        }
+        assertThat(containsText).isEqualTo(true);
+    }
+
+    private WebElement findSubjectRow(String subject){
+        int currentRow = 0, currentColumn = 0;
+        String text;
+        WebElement rowContainingSubject = null;
+
+        WebDriver driver = getDriver();
+
+        List<WebElement> rows = driver.findElements(By.cssSelector("[id='tmtab_1'] tr"));
+        for (WebElement row: rows) {
+            System.out.println("------------------------------------------------------------------------------------------------------");
+            text = row.getText();
+            System.out.println("Row number " + (currentRow + 1) + " contains text: " + text);
+
+            List<WebElement> columns = row.findElements(By.tagName("td"));
+
+            for (WebElement column : columns) {
+                if(currentColumn == 2){
+                    System.out.println("Column " + currentColumn + " : " + text);
+                    text = column.getText();
+                    if(text.contains(subject)){
+                        rowContainingSubject = row;
+                    }
+                    currentColumn++;
+                }
+                currentColumn++;
+            }
+            currentRow++;
+            currentColumn = 0;
+        }
+
+        return rowContainingSubject;
+    }
+
+    private String findFinalGrade(WebElement rowContainingSubject){
+        int currentColumn = 0;
+        String text;
+
+        if(rowContainingSubject == null){
+            System.out.println("Subject is not presented in selected period.");
+        }else{
+            List<WebElement> columns = rowContainingSubject.findElements(By.tagName("td"));
+            for (WebElement column : columns) {
+                if(currentColumn == 7){
+                    text = column.getText();
+                    System.out.println("Column " + currentColumn + " : " + text);
+
+                    return text;
+                }
+                currentColumn++;
+            }
+
+        }
+
+        return null;
+    }
+}
--- src/test/java/starter/stepdefinitions/FindFinalGradeStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/FindFinalGradeStepDefinitions.java
@@ -3,16 +3,9 @@ package starter.stepdefinitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import net.serenitybdd.core.annotations.findby.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import starter.enteringData.ChangeTimePeriod;
+import starter.matchers.CheckIfGradeMatches;
 import starter.navigation.NavigateTo;
-
-import java.util.List;
-
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindFinalGradeStepDefinitions {
     @When("she navigates to e-study record")
@@ -27,65 +20,7 @@ public class FindFinalGradeStepDefinitions {
 
     @Then("she found {string} from {string}")
     public void checkGrade(String grade, String subject) {
-        int currentRow = 0, currentColumn = 0;
-        String text;
-        boolean containsText = false;
-        WebElement rowContainingSubject = null;
-
-        WebDriver driver = getDriver();
-
-        //hladam spravny riadok
-        List<WebElement> rows = driver.findElements(By.cssSelector("[id='tmtab_1'] tr"));
-        for (WebElement row: rows) {
-            System.out.println("------------------------------------------------------------------------------------------------------");
-            text = row.getText();
-            System.out.println("Row number " + (currentRow + 1) + " contains text: " + text);
-
-            List<WebElement> columns = row.findElements(By.tagName("td"));
-
-            for (WebElement column : columns) {
-                if(currentColumn == 2){
-                    System.out.println("Column " + currentColumn + " : " + text);
-                    text = column.getText();
-                    if(text.contains(subject)){
-                        rowContainingSubject = row;
-                    }
-                    currentColumn++;
-                }
-                currentColumn++;
-            }
-            currentRow++;
-            currentColumn = 0;
-        }
-
-        //uz mam najdeny spravny riadok, urobim v nom co potrebujem: znamka je v stlpci c. 6
-        if(rowContainingSubject == null){
-            System.out.println("Subject is not presented in selected period.");
-            containsText = false;
-        }else{
-            currentColumn = 0;
-
-            List<WebElement> columns = rowContainingSubject.findElements(By.tagName("td"));
-            for (WebElement column : columns) {
-                if(currentColumn == 7){
-                    text = column.getText();
-                    System.out.println("Column " + currentColumn + " : " + text);
-
-                    if(text.contains("(" + grade + ")")){
-                        System.out.println("Column " + currentColumn + " contains expected grade \"" + grade + "\"");
-                        containsText = true;
-                    }
-                    else{
-                        System.out.println("Column " + currentColumn + " DOESN'T contain expected grade \"" + grade + "\"");
-                        System.out.println("Expected: " + grade + ", Is: " + text);
-                        containsText = false;
-                    }
-                    currentColumn++;
-                }
-                currentColumn++;
-            }
-
-        }
-        assertThat(containsText).isEqualTo(true);
+        CheckIfGradeMatches condition = new CheckIfGradeMatches();
+        condition.check(grade, subject);
     }
 }
